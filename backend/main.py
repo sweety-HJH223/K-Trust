9588f1f60b878bac93c8f4fd68a2628134cd3034
sweety-HJH223 <indiraganeshsubha@gmail.com>
2026-05-23 23:01:20 +0530

    fix: CORS update and deployment files

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,10 @@ app = FastAPI(title="K-Trust API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://project-4xzgu-blush.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
